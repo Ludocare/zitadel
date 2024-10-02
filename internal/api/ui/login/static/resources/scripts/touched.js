7284f95575baf347464af7af41042d512321b7f6
Jean-Philippe Novent <jean-philippe.novent@ludocare.com>
2024-10-02 10:26:13 +0200

    add totp input format specific

--- internal/api/ui/login/static/resources/scripts/touched.js
+++ internal/api/ui/login/static/resources/scripts/touched.js
@@ -1,13 +1,19 @@
 let inputs = document.getElementsByTagName("input");
 
-if (inputs && inputs.length) {
+if (inputs?.length) {
   for (let input of inputs) {
+    if (input.getAttribute("autocomplete") === "one-time-code") continue; // Skip inputs with autocomplete="one-time-code"
+
     input.addEventListener("focus", () => {
       input.classList.add("lgn-focused");
     });
+
     input.addEventListener("blur", () => {
       input.classList.add("lgn-touched");
       input.classList.remove("lgn-focused");
     });
   }
 }
+
+const input = document.querySelector('[autocomplete=one-time-code');
+input.addEventListener('input', () => input.style.setProperty('--_otp-digit', input.selectionStart));
